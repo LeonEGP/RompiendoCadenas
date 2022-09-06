eb036c37abeb6c00fd5e4bedf9bdf28d596f43b4
León G <77093758+LeonEGP@users.noreply.github.com>
2022-09-06 14:23:10 -0500

    Código comentado

--- CadenaPicada.cpp
+++ CadenaPicada.cpp
@@ -1,9 +1,9 @@
-
 // ---------- DATOS DE ENTREGA ---------- //
 //Programa que: Implementa la un algoritmo de Hash String.
 //Programadores: León Emiliano García Pérez [A00573074], Carla Morales López [A01639225].
 //Fecha de entrega: Martes 06 de Septiembre de 2022.
 
+//Inclusión de librerías.
 #include <iostream>
 #include <string>
 #include <vector>
@@ -11,24 +11,26 @@
 #include <sstream>
 #include <iomanip>
 
+//Ajuste a estandar.
 using namespace std;
 
-void espacio() {
+//Función que imprime en consola un salto de línea, no recibe parámetros y no tiene valor de retorno.
+void espacio() { //Complejidad Computacional: O(1).
     cout << endl;
 }
 
-bool multiplicidad(int numero, int multiplo) {
-
+//Función que verifica la multiplicidad entre dos números, recibe dos valores enteros y retorna un valor booleano.
+bool multiplicidad(int numero, int multiplo) { //Complejidad Computacional: O(1).
     if (numero % multiplo == 0) {
         return true;
     }
     else {
         return false;
     }
-
 }
 
-int multiploCercano(int numero, int base) {
+//Función que encuentra el múltiplo más cercano a un número, recibe un entero como número y un entero como base, retorna el múltiplo entero más cercano. 
+int multiploCercano(int numero, int base) { //Complejidad Computacional: O(1).
     
     numero = numero + base / 2;
     numero = numero - (numero % base);
@@ -36,31 +38,34 @@ int multiploCercano(int numero, int base) {
     if (base > numero) {
         return base;
     }
-
     return numero;
 }
 
-string aMayuscula(string cadena) {
+//Función que convierte un string a una string en solo mayúsculas, recibe como parámetro el string a convertir y retorna el string en mayúsculas.
+string aMayuscula(string cadena) { //Complejidad Computacional: O(n), siendo n la cantidad de caracteres del string.
     for (int i = 0; i < cadena.length(); i++) {
         cadena[i] = toupper(cadena[i]);
     }
     return cadena;
 }
 
-void hashString(string nombreArchivo, int n) {
+//Función que muestra el hasheo de un archivo de texto, recibe como parámetro un strig que es el nombre del archivo.
+void hashString(string nombreArchivo, int n) { //Complejidad Computacional: O(n), siendo n la cantidad de caracteres del archivo de texto.
 
+    //Declaración/Inicialización de variables.
     int longitudString;
     int columnas;
-    vector <vector <char>> tabla;
+    char auxiliar;
     string linea;
+    string hashing;
     vector <long long int> resultado;
     vector <string> hexadecimal;
-    string hashing;
-    char auxiliar;
+    vector <vector <char>> tabla;
+    
+    longitudString = n / 4; //Dentro de lo solicitado se menciona que el output es de tamaño n/4.
+    columnas = n; //Variable que toma el valor de n, por practicidad.
 
-    longitudString = n / 4;
-    columnas = n;
-
+    //Para llenar la tabla con los valores faltantes de cada linea, fue solicitado que se ingresara el valor de n, como solo se puede tomar un char del valor entero, se opta por tomar el valor de las decenas como representativo.
     if (n >= 10 && n < 19) {
         auxiliar = '1';
     }
@@ -80,43 +85,47 @@ void hashString(string nombreArchivo, int n) {
         auxiliar = '6';
     }
 
+    //Formación de la tabla con n columnas.
     for (int i = 0; i < columnas; i++) {
         vector <char> columna;
         tabla.push_back(columna);
     }
 
+    //Apertura del archivo a analizar. 
     ifstream archivo(nombreArchivo.c_str());
 
+    //Lectura de cada renglón del archivo. 
     while (getline(archivo, linea)) {
 
-        int iterador;
+        int iterador; //Iterador que se actualiza desde 0 a n.
         iterador = 0;
 
+        //Ciclo que va a acomodar el char de cada string (renglón).
         for (int i = 0; i < linea.length(); ++i) {
 
             char caracter;
             caracter = linea[i];
-            tabla[iterador].push_back(caracter);
+            tabla[iterador].push_back(caracter); 
 
+            //Actualización del iterador.
             if ((iterador + 1) == columnas) {
                 iterador = 0;
             }
             else {
                 iterador++;
             }
-
         }
 
+        //Comprueba si hay espacios faltantes, si es así: se rellena con el char valor representativo de n.
         if (iterador != 0) {
-
             for (int j = iterador; j < columnas; j++) {
                 tabla[j].push_back(auxiliar);
             }
-
         }
 
     }
 
+    //Ciclo que suma cada columna de la tabla, obtiene de la sumatoria %256 y lo almacena en el vector de resultados. 
     for (int i = 0; i < columnas; i++) {
 
         long long int suma;
@@ -130,47 +139,54 @@ void hashString(string nombreArchivo, int n) {
         resultado.push_back(suma);
     }
 
+    //Ciclo que convierte del vector de resultados anterior, cada valor a hexadecimal, almacenándolos en dicho vector. 
     for (int i = 0; i < resultado.size(); i++) {
         
         long long int valor;
         string converso;
         valor = resultado[i];
-
         ostringstream os;
-        //os << "0x" << setfill('0') << setw(longitudString) << hex << valor;
-        os << setfill('0') << setw(2) << hex << valor;
 
-        converso = os.str();
-        converso = aMayuscula(converso);
+        // Si se quisiera una notación correcta, respecto a lo tradicional -> os << "0x" << setfill('0') << setw(longitudString) << hex << valor;
+        os << setfill('0') << setw(2) << hex << valor; //Considerando que se busca notación a dos valores en hexadecimal. 
+
+        converso = os.str(); //Se convierte a string. 
+        converso = aMayuscula(converso); //Se convierte a mayúsculas, ya que es requerido que sea en mayúsculas la notación representativa en hexadecimal. 
         hexadecimal.push_back(converso);
     }
 
+    //Ciclo que concatena los anteriores valores hexadecimales en un solo string.
     for (int i = 0; i < hexadecimal.size(); i++) {
         hashing += hexadecimal[i];
     }
 
-    //hashing = hashing.substr(hashing.size() - longitudString);
+    //Dentro de lo solicitado se solicita que el output sea de longitud n/4, considerando que la concatenación de los valores hexadecimales de las colummas es de longitud: 2n,
+    // Entonces es que se opta por reducir el tamaño de dicha concatenación, tomando los primeros n valores del string resultado, ya que es en dicho espacio donde se encuentra más información. 
+    
+    // Esto es si se quisieran los últimos n caracteres -> hashing = hashing.substr(hashing.size() - longitudString); //Pérdida aún mayor de información. 
     hashing = hashing.substr(0, longitudString);
 
+    //Impresión en consola del hashing. 
     espacio();
     cout << "RESULTADO HASHING: " << endl;
     cout << hashing << endl;
-
 }
 
+//Función main que ejecuta el programa, no recibe parámetros, retorna un entero [return 0].
 int main() {
 
+    // ---------- DECLARACIÓN DE VARIABLES ---------- //
     string archivo;
     int n;
 
+    // ---------- INICIA PROGRAMA ---------- //
     cout << "INGRESE EL NOMBRE DEL ARCHIVO: " << endl;
     cin >> archivo;
-
     espacio();
-
     cout << "INGRESE EL VALOR N: " << endl;
     cin >> n;
 
+    // ---------- DETECCIÓN DE ERRORES DE INPUT ---------- //
     if (n < 16) {
         cout << "VALOR DE N MENOR AL ESPERADO, AJUSTANDO A 16" << endl;
         n = 16;
@@ -185,8 +201,9 @@ int main() {
         }
     }
 
+    // ---------- HASHEO DEL ARCHIVO ---------- //
     hashString(archivo,n);
 
+    // ---------- TERMINA PROGRAMA ---------- //
     return 0;
 }
-
